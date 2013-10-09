9129774f3252b7c7c1e43ba11ee83c60415e6381
Danny Kirschner <rkirschner1377@gmail.com>
2013-10-09 17:42:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pandata::Application.config.secret_key_base = '686fb46a97c3d4a4ae61d522903197412c6a75ab8322f34f5fa37d9b1dc39d940c9bfedb46fb1713b9ca35877025eb8eaecfdb915b28537d66304b4e2337b888'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pandata::Application.config.session_store :cookie_store, key: '_pandata_session'
