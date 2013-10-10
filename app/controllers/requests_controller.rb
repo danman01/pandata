3a84f6f7ccd58741c03fe68dd8974f5764f08f0d
ewaltman <evan@crowdsurf.io>
2013-10-10 16:20:40 -0400

    worked on requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -11,21 +11,33 @@ class RequestsController < ApplicationController
     if scraper.class == Array && scraper.empty?
       # return not found
       response = {:status=>"not found",:email=>params[:email]}
-    else
+    else 
       # the basics
       response = {:status=>"found",:email=>params[:email]}
       # what to get from pandata?
       #
-      # recent activity
-      response.merge!({:recent_activity=>scraper.recent_activity})
-      #
-      # stations
-      response.merge!({:stations=>scraper.stations, :playing_station=>scraper.playing_station})
-      # followers, following
-      response.merge!({:followers=>scraper.followers,:following=>scraper.following})
-      # bookmarks
-      response.merge!({:bookmarks => scraper.bookmarks})
-      response.merge!({:likes => scraper.likes})
+      # check which tags we want
+      if params[:all]
+        # debugger
+        # in the console with debugger: type eval varname to see what varname is
+
+        # recent activity
+        response.merge!({:recent_activity=>scraper.recent_activity})
+        #
+        # stations
+        response.merge!({:stations=>scraper.stations, :playing_station=>scraper.playing_station})
+        # followers, following
+        response.merge!({:followers=>scraper.followers,:following=>scraper.following})
+        # bookmarks
+        response.merge!({:bookmarks => scraper.bookmarks})
+        response.merge!({:likes => scraper.likes})
+      end
+      if params[:recent_activity]
+        # get recent activity
+      end
+      #if condition
+        
+      #end
     end
     
     @response = {:pandata => response}
